a540a56b4fc2dd2ea7798d0d8a1ac66d21884f78
taher.chabaane <taher.chabaane.1@gmail.com>
2021-06-16 18:45:35 +0200

    hooks project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
+import React, { useState } from 'react';
+import { moviesData } from './moviesData';
+import SearchMovie from './components/SearchMovie/SearchMovie';
+import MoviesList from './components/MoviesList';
+import AddMovie from './components/AddMovie/AddMovie';
+
 import './App.css';
-import MovieList from './components/MovieList';
-function App(){
-    return(
-    <div className ="App">
-    <MovieList/>
+
+function App() {
+  const [moviesList, setMoviesList] = useState(moviesData);
+  const [nameSearch, setNameSearch] = useState('');
+  const [ratingSearch, setRatingSearch] = useState(1);
+
+  const addNewMovie = (e, newMovie) => {
+    e.preventDefault();
+    setMoviesList([...moviesList, newMovie]);
+  };
+
+  return (
+    <div className="App">
+      <SearchMovie
+        setNameSearch={setNameSearch}
+        ratingSearch={ratingSearch}
+        setRatingSearch={setRatingSearch}
+      />
+      <MoviesList
+        moviesList={moviesList}
+        nameSearch={nameSearch}
+        ratingSearch={ratingSearch}
+      />
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <AddMovie addNewMovie={addNewMovie} />
+      </div>
     </div>
   );
-  }
-
+}
 
 export default App;
+
+
+
